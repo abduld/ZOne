37fd3d3f1d3a2369b7062fe64300000ec03a8874
Abdul Dakkak <abduld@wolfram.com>
2014-04-01 12:28:37 -0500

    code refactoring

---
+++ runtime/src/zMalloc.h
@@ -0,0 +1,103 @@
+
+#ifndef __ZMALLOC_H__
+#define __ZMALLOC_H__
+
+enum {
+  zMalloc_fieldSize = 0,
+  zMalloc_fieldCount
+};
+
+#define zMalloc_padding (zMalloc_fieldCount * sizeof(size_t))
+#define zMalloc_address(mem) (mem - zMalloc_padding)
+#define zMalloc_getSize(mem) ((size_t) zMemoryAddress(mem)))[zMemoryField_size]
+#define zMalloc_setSize(mem, sz) (zMemory_getSize(mem) = sz)
+
+static inline void *xMalloc(size_t sz) {
+  void *mem = NULL;
+  if (sz != 0) {
+    mem = malloc(sz + zMalloc_padding);
+  }
+  if (mem != NULL) {
+    mem = mem + zMalloc_padding;
+    zMalloc_setSize(mem, sz);
+    return mem;
+  } else {
+    return NULL;
+  }
+}
+
+static inline void xFree(void *mem) {
+  if (mem != NULL) {
+    free(zMalloc_address(mem));
+  }
+  return;
+}
+
+static inline void *xRealloc(void *mem, size_t sz) {
+  if (mem == NULL) {
+    return NULL;
+  } else if (sz == 0) {
+    xFree(mem);
+    return NULL;
+  } else {
+    void *tm = zMalloc_address(mem);
+    void *res = realloc(tm, sz);
+    if (res != NULL) {
+      res += zMalloc_padding;
+      zAssert(res != NULL);
+      zMalloc_setSize(res, sz);
+    }
+    return res;
+  }
+}
+
+static inline void *xcuMalloc(size_t sz) {
+  void *mem = NULL;
+  if (sz != 0) {
+    cudaError_t err = cudaMallocHost((void **) &mem, sz + zMalloc_padding);
+    if (zSuccessQ(err)) {
+      mem = mem + zMalloc_padding;
+      zMalloc_setSize(mem, sz);
+      return mem;
+    }
+  }
+  return NULL;
+}
+
+static inline void xcuFree(void *mem) {
+  if (mem != NULL) {
+    cudaError_t err = cudaFreeHost(zMalloc_address(mem));
+    checkSuccess(err);
+  }
+  return;
+}
+
+static inline void *xcuRealloc(void *mem, size_t sz) {
+  if (mem == NULL) {
+    return NULL;
+  } else if (sz == 0) {
+    xFree(mem);
+    return NULL;
+  } else {
+    void *res = xcuMalloc(sz);
+    zAssert(res != NULL);
+    if (res != NULL) {
+      cudaError_t err = cudaMemcpy(res, mem, zMalloc_getSize(mem), cudaMemcpyHostToHost);
+      checkSuccess(err);
+    }
+    xcuFree(mem);
+    return res;
+  }
+}
+
+#define zNew(type) ((type *)zMalloc(sizeof(type)))
+#define zNewArray(type, len) ((type *)zMalloc((len) * sizeof(type)))
+#define zMalloc(sz) xcuMalloc(sz)
+#define zDelete(var) zFree(var)
+#define zFree(var)   do { xcuFree(var); var = NULL; } while(0)
+#define zRealloc(var, newSize) xcuRealloc(var, newSize)
+#define zReallocArray(t, m, n) ((t *)xcuRealloc(m, n * sizeof(t)))
+
+
+#endif /* __ZMALLOC_H__ */
+
--- runtime/src/zUtils.h
+++ runtime/src/zUtils.h
@@ -13,9 +13,6 @@
 #define zExit()  do { zAssert(0); exit(1); } while(0)
 #define zPrint(msg) do { std::cout << msg << std::endl; } while (0)
 
-#define zMemoryAddress(mem) (mem - sizeof(size_t)
-#define zMemorySize(mem) ((size_t) zMemoryAddress(mem)))[0]
-
 #ifdef Z_CONFIG_DEBUG
 #define zAssert(cond) assert(cond)
 #define zAssertMessage(msg, cond)                                             \
@@ -32,88 +29,6 @@
 #define zError(msg) zPrint(msg)
 #endif /* WB_DEBUG */
 
-static inline void *xMalloc(size_t sz) {
-  void *mem = NULL;
-  if (sz != 0) {
-    mem = malloc(sz + sizeof(size_t));
-  }
-  if (mem != NULL) {
-    ((size_t) mem)[0] = sz;
-    return mem + sizeof(size_t);
-  } else {
-    return NULL;
-  }
-}
-
-static inline void xFree(void *mem) {
-  if (mem != NULL) {
-    free(zMemoryAddress(mem));
-  }
-  return;
-}
-
-static inline void *xRealloc(void *mem, size_t sz) {
-  if (mem == NULL) {
-    return NULL;
-  } else if (sz == 0) {
-    xFree(mem);
-    return NULL;
-  } else {
-    void *tm = mem - sizeof(size_t);
-    void *res = realloc(tm, sz);
-    if (res != NULL) {
-      ((size_t) res)[0] = sz;
-      zAssert(res != NULL);
-    }
-    return res;
-  }
-}
-
-static inline void *xcuMalloc(size_t sz) {
-  void *mem = NULL;
-  if (sz != 0) {
-    cudaError_t err = cudaMallocHost((void **) &mem, sz + sizeof(size_t));
-    if (zSuccessQ(err)) {
-      ((size_t) mem)[0] = sz;
-      return mem + sizeof(size_t);
-    }
-  }
-  return NULL;
-}
-
-static inline void xcuFree(void *mem) {
-  if (mem != NULL) {
-    cudaError_t err = cudaFreeHost(zMemoryAddress(mem));
-    checkSuccess(err);
-  }
-  return;
-}
-
-static inline void *xcuRealloc(void *mem, size_t sz) {
-  if (mem == NULL) {
-    return NULL;
-  } else if (sz == 0) {
-    xFree(mem);
-    return NULL;
-  } else {
-    void *res = xcuMalloc(sz);
-    zAssert(res != NULL);
-    if (res != NULL) {
-      cudaError_t err = cudaMemcpy(res, mem, zMemorySize(mem), cudaMemcpyHostToHost);
-      checkSuccess(err);
-    }
-    xcuFree(mem);
-    return res;
-  }
-}
-
-#define zNew(type) ((type *)zMalloc(sizeof(type)))
-#define zNewArray(type, len) ((type *)zMalloc((len) * sizeof(type)))
-#define zMalloc(sz) xcuMalloc(sz)
-#define zDelete(var) zFree(var)
-#define zFree(var)   do { xcuFree(var); var = NULL; } while(0)
-#define zRealloc(var, newSize) xcuRealloc(var, newSize)
-#define zReallocArray(t, m, n) ((t *)xcuRealloc(m, n * sizeof(t)))
-
+#include "zMalloc.h"
 
 #endif /* __ZUTILS_H__ */
