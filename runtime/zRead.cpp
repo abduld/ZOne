ec6eaba0d702657d71c77b8fa3deb597a19d18c4
abduld <abduld@wolfram.com>
2014-04-10 07:24:07 -0500

    renames

--- runtime/zRead.cpp
+++ runtime/zRead.cpp
@@ -2,69 +2,69 @@
 
 #include "z.h"
 
-void afterRead(uv_fs_t * req) {
-  uv_fs_req_cleanup(req);
+void afterReadChunk(uv_fs_t * req) {
+  zMemory_t * pmem = (zMemory_t *) req->data;
+  zAssert(pmem != NULL);
+  zMemory_t mem = *pmem;
+  zState_t st = zMemory_getState(mem);
+  uv_loop_t * loop = zState_getLoop(st);
   if (req->result < 0) {
-    fprintf(stderr, "Read error: %s\n", uv_strerror(uv_last_error(uv_default_loop())));
+    uv_err_t uvErr = uv_last_error(loop);
+    zState_setError(st, uvErr);
   } else if (req->result == 0) {
-      zFile_close(file);
+    // TODO: think about what if not all have finished reading
+    // zFile_close(fileName);
   }
-  zMemory_t mem = req->data;
   // spin until memory has been allocated
   while (!zMemory_deviceMemoryAllocatedQ(mg)) {}
   zMemory_copyToDevice(mem);
+  uv_fs_req_cleanup(req);
 }
 
-static zMemoryGroup_t zReadArray(zState_t st, const char *file,
+static zMemoryGroup_t zReadArray(zState_t st, const char *fileName,
                                  zMemoryType_t typ, int rank, size_t *dims) {
-  zBool deviceMemoryAllocatedQ;
-  size_t byteCount;
-  size_t chunkByteCount;
-  uv_work_t work_req;
-  ReadArrayWork_t workData;
-  uv_mutex_t mutexDeviceMemoryAllocatedQ;
 
   uv_loop_t *loop = zState_getLoop(st);
 
   zMemoryGroup_t mg = zMemoryGroup_new(st, type, rank, dims);
-  deviceMemoryAllocatedQ = zFalse;
 
-  zFile_t file = zFile_open(st, file, S_IREAD);
-  // TODO: one can interleave the host memory alloc with the file read here
+  zFile_t file = zFile_open(st, fileName, S_IREAD);
+  // TODO: one can interleave the host memory alloc with the fileName read here
   // TODO: need to know if malloc is thread safe...
 
   size_t offset = 0;
   for (int ii = 0; ii < zMemoryGroup_getMemoryCount(mg); ii++) {
       zMemory_t mem = zMemoryGroup_getMemory(mg, ii);
       size_t memBytecount = zMemory_getByteCount(mem);
-      zFile_readChunk(file, zMemory_getHostMemory(mem), memBytecount, offset, afterRead, mem);
-      offset += memByteCount;
+      zFile_readChunk(file, zMemory_getHostMemory(mem), memBytecount, offset,
+        afterReadChunk, &zMemoryGroup_getMemory(mg, ii));
+      offset += memBytecount;
   }
 
   return mg;
 }
 
-zMemoryGroup_t zReadBit8Array(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadBit8Array(zState_t st, const char *fileName, int rank,
                               size_t *dims) {
-  return zReadArray(st, file, zMemoryType_bit8, rank, dims);
+  return zReadArray(st, fileName, zMemoryType_bit8, rank, dims);
 }
 
-zMemoryGroup_t zReadInt32Array(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadInt32Array(zState_t st, const char *fileName, int rank,
                                size_t *dims) {
-  return zReadArray(st, file, zMemoryType_int32, rank, dims);
+  return zReadArray(st, fileName, zMemoryType_int32, rank, dims);
 }
 
-zMemoryGroup_t zReadInt64Array(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadInt64Array(zState_t st, const char *fileName, int rank,
                                size_t *dims) {
-  return zReadArray(st, file, zMemoryType_int64, rank, dims);
+  return zReadArray(st, fileName, zMemoryType_int64, rank, dims);
 }
 
-zMemoryGroup_t zReadFloatArray(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadFloatArray(zState_t st, const char *fileName, int rank,
                                size_t *dims) {
-  return zReadArray(st, file, zMemoryType_float, rank, dims);
+  return zReadArray(st, fileName, zMemoryType_float, rank, dims);
 }
 
-zMemoryGroup_t zReadDoubleArray(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadDoubleArray(zState_t st, const char *fileName, int rank,
                                 size_t *dims) {
-  return zReadArray(st, file, zMemoryType_double, rank, dims);
+  return zReadArray(st, fileName, zMemoryType_double, rank, dims);
 }
--- runtime/zRead.h
+++ runtime/zRead.h
@@ -3,19 +3,19 @@
 #ifndef __ZREAD_H__
 #define __ZREAD_H__
 
-/* We are going to assume that the format of the files is that
+/* We are going to assume that the format of the fileNames is that
 the first line will contain the dimensions and the second
 line contains the data comma sperated */
 
-zMemoryGroup_t zReadBit8Array(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadBit8Array(zState_t st, const char *fileName, int rank,
                               size_t *dims);
-zMemoryGroup_t zReadInt32Array(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadInt32Array(zState_t st, const char *fileName, int rank,
                                size_t *dims);
-zMemoryGroup_t zReadInt64Array(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadInt64Array(zState_t st, const char *fileName, int rank,
                                size_t *dims);
-zMemoryGroup_t zReadFloatArray(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadFloatArray(zState_t st, const char *fileName, int rank,
                                size_t *dims);
-zMemoryGroup_t zReadDoubleArray(zState_t st, const char *file, int rank,
+zMemoryGroup_t zReadDoubleArray(zState_t st, const char *fileName, int rank,
                                 size_t *dims);
 
 #endif /* __ZREAD_H__ */
