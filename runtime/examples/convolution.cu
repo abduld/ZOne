e42440182910722ea82cd5dbe6af34923ffe7fc2
abduld <abduld@wolfram.com>
2014-05-01 22:47:02 -0500

    .

--- runtime/examples/convolution.cu
+++ runtime/examples/convolution.cu
@@ -2,8 +2,12 @@
 
 #include "z.h"
 
-typename <int kernelWidth>
+#define KERNEL_RADIUS 1
+#define KERNEL_WIDTH (2*KERNEL_RADIUS + 1)
+#define BLOCK_SIZE  16
+
 __global__ void convolutionKernel(unsigned char * output, const char * input, float * kernel, int inputWidth, int inputHeight) {
-	__shared__ float sKernel[kernelWidth][kernelWidth];
+	__shared__ float sKernel[KERNEL_WIDTH][KERNEL_WIDTH];
+	__shared__ float sKernel[BLOCK_SIZE + 2 * KERNEL_RADIUS][BLOCK_SIZE + 2 * KERNEL_RADIUS];
 	
 }
